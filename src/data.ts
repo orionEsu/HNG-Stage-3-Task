6db9de1a6cab477eafd20fbc2cd45f430c09cb82
orionEsu <orionesu.dev@gmail.com>
2023-09-21 11:36:46 +0100

    optimized image

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const data = [
 		id: 1,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-1.png',
+			gallery: '/assets/product-1.webp',
 		},
 	},
 
@@ -11,14 +11,14 @@ export const data = [
 		id: 2,
 		gender: 'children',
 		images: {
-			gallery: '/assets/product-2.png',
+			gallery: '/assets/product-2.webp',
 		},
 	},
 	{
 		id: 3,
 		gender: 'children',
 		images: {
-			gallery: '/assets/product-3.png',
+			gallery: '/assets/product-3.webp',
 		},
 	},
 
@@ -26,63 +26,63 @@ export const data = [
 		id: 4,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-4.png',
+			gallery: '/assets/product-4.webp',
 		},
 	},
 	{
 		id: 5,
 		gender: 'men',
 		images: {
-			gallery: '/assets/product-5.png',
+			gallery: '/assets/product-5.webp',
 		},
 	},
 	{
 		id: 6,
 		gender: 'children',
 		images: {
-			gallery: '/assets/product-6.jpg',
+			gallery: '/assets/product-6.webp',
 		},
 	},
 	{
 		id: 7,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-7.png',
+			gallery: '/assets/product-7.webp',
 		},
 	},
 	{
 		id: 8,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-8.png',
+			gallery: '/assets/product-8.webp',
 		},
 	},
 	{
 		id: 9,
 		gender: 'men',
 		images: {
-			gallery: '/assets/product-9.jpg',
+			gallery: '/assets/product-9.webp',
 		},
 	},
 	{
 		id: 10,
 		gender: 'men',
 		images: {
-			gallery: '/assets/product-10.jpg',
+			gallery: '/assets/product-10.webp',
 		},
 	},
 	{
 		id: 11,
 		gender: 'men',
 		images: {
-			gallery: '/assets/product-11.jpg',
+			gallery: '/assets/product-11.webp',
 		},
 	},
 	{
 		id: 12,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-12.png',
+			gallery: '/assets/product-12.webp',
 		},
 	},
 
@@ -90,28 +90,28 @@ export const data = [
 		id: 13,
 		gender: 'men',
 		images: {
-			gallery: '/assets/product-13.jpg',
+			gallery: '/assets/product-13.webp',
 		},
 	},
 	{
 		id: 14,
 		gender: 'women',
 		images: {
-			gallery: '/assets/product-14.png',
+			gallery: '/assets/product-14.webp',
 		},
 	},
 	{
 		id: 15,
 		gender: 'children',
 		images: {
-			gallery: '/assets/product-17.jpg',
+			gallery: '/assets/product-17.webp',
 		},
 	},
 	{
 		id: 16,
 		gender: 'children',
 		images: {
-			gallery: '/assets/product-18.png',
+			gallery: '/assets/product-18.webp',
 		},
 	},
 ];
