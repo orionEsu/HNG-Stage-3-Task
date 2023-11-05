abde4ce71900f1457e705ba8021d99985756b580
orionEsu <orionesu.dev@gmail.com>
2023-11-05 13:22:50 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Homepage from './pages/Homepage.tsx';
+// import Homepage from './pages/Homepage.tsx';
 import './index.css';
 import Login from './pages/Login.tsx';
-import PrivateRoute from './routing/PrivateRoute.tsx';
+// import PrivateRoute from './routing/PrivateRoute.tsx';
 
 const router = createBrowserRouter([
 	{ path: '/', element: <Login /> },
 
-	{
-		element: <PrivateRoute />,
-		children: [{ path: '/home', element: <Homepage /> }],
-	},
+	// {
+	// 	element: <PrivateRoute />,
+	// 	children: [{ path: '/home', element: <Homepage /> }],
+	// },
 ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
