6bbe67975da5356ab558a29a889c9db29d281274
orionEsu <orionesu.dev@gmail.com>
2023-11-05 16:38:12 +0100

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ const Home = () => {
 			<div className='home home__body'>
 				<div className='container'>
 					<img
-						src='../../public/assets/burglar_2595242b (1).jpg'
+						src='/dist/assets/burglar_2595242b (1).jpg'
 						alt=''
 					/>
 					<p>
